4b3e26c37845eece02a77af8de86969f64226259
atepaevm <atepaev_mihail@mail.ru>
2018-04-05 00:25:01 +0300

    few persons functions

--- server.js
+++ server.js
@@ -1,4 +1,38 @@
 let cassandraApi = require('./src/cassandra_api');
 let neo4jApi = require('./src/neo4j_api');
+/**
+ * working functions:
+ cassandraApi.insertPerson("0","boss","Test","Tester",null,"Director",100,"Active");
 
-let result = cassandraApi.runProcess();
+ ids=['1','2'];
+ bosses=['boss1','boss2'];
+ names=['Misha','Masha'];
+ surnames=['sur1','sur2'];
+ nicks=[null,null];
+ pos=['Director','Manager'];
+ sal=[100,500];
+ stats=['Active','Dead'];
+ let res=cassandraApi.insertPersons(ids,bosses,names,surnames,nicks,pos,sal,stats);
+
+ removePerson('2');
+ */
+cassandraApi.init();
+try{
+    //let res=cassandraApi.selectTop10Persons("2");
+    //console.log(res);
+    let res=cassandraApi.getAllPersons().then();
+    console.log(res);
+}
+catch (e) {
+    console.log(e.stack);
+}
+console.log("After all");
+let entry=[{
+    name:'Myname'
+},{
+    name:'Myname'
+}]
+
+//console.log(cassandraApi.returnHumanLogs(null));
+//console.log('server ends');
+//let result = cassandraApi.runProcess();
